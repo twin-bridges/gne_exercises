ec5ee579d92d4d33111d761d4bb829c1e8203a80
Kirk Byers <ktbyers@twb-tech.com>
2024-05-16 19:22:07 -0700

    Adding 'show_run.py program

---
+++ lesson7/show_run.py
@@ -0,0 +1,25 @@
+import os
+from datetime import datetime
+from getpass import getpass
+from netmiko import ConnectHandler
+
+password = os.getenv("NETMIKO")
+if not password:
+    password = getpass("Enter your Netmiko password: ")
+
+# cisco1.bogus.com does not resolve, so use it as fictional domain
+device = {
+    "device_type": "cisco_xe",
+    "host": "cisco1.bogus.com",
+    "username": "jmorrow",
+    "password": password,
+}
+
+start_time = datetime.now()
+with ConnectHandler(**device) as nc:
+    data = nc.send_command("show run")
+    print(data)
+
+# print(data)
+end_time = datetime.now()
+print(f"{end_time - start_time}")
