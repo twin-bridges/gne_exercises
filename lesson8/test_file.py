45c93e97c22627e70614f66fbcba6722d4bea008
Kirk Byers <ktbyers@twb-tech.com>
2024-07-09 12:44:23 -0700

    rebase interactive commit3 (amended test_file.py)

--- lesson8/test_file.py
+++ lesson8/test_file.py
@@ -1,4 +1,14 @@
 from random import randint
+HEADS = 1
+TAILS = 2
 
+print("\nTen coin flips:")
+print("-" * 30)
 for _ in range(10):
-    print(randint(1,2))
+    if randint(1,2) == HEADS:
+        print("HEADS")
+    else:
+        print("TAILS")
+
+print("-" * 30)
+print()
