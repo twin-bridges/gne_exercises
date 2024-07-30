2dd8438b7773ee4f268664d4720dfa8b7220ad77
Kirk Byers <ktbyers@twb-tech.com>
2024-07-29 17:46:35 -0700

    Fixing linting

--- lesson8/test_file.py
+++ lesson8/test_file.py
@@ -1,11 +1,12 @@
 from random import randint
+
 HEADS = 1
 TAILS = 2
 
 print("\nTen coin flips:")
 print("-" * 30)
 for _ in range(10):
-    if randint(1,2) == HEADS:
+    if randint(1, 2) == HEADS:
         print("HEADS")
     else:
         print("TAILS")
